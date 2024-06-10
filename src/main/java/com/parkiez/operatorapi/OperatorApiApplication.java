1d9bd56036f2a086e5772ca9a4ed592896a29108
Aditya Dhamale <aditya1dhamale1@gmail.com>
2024-06-10 17:20:40 +0530

    intial files

---
+++ src/main/java/com/parkiez/operatorapi/OperatorApiApplication.java
@@ -0,0 +1,14 @@
+package com.parkiez.operatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperatorApiApplication.class, args);
+		System.out.println("started . . .");
+	}
+
+}
